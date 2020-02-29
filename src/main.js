ca80db180cbce072dfdede89ce5435378f17e48d
Mauveine <leohirja@gmail.com>
2020-02-29 11:20:42 +0100

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from '~/App.vue'
+import { Page, ActionBar, GridLayout, Button, Img, Label } from 'nativescript-vue-web'
 import store from './store'
-import './registerServiceWorker'
+
+Vue.component('Page', Page)
+Vue.component('ActionBar', ActionBar)
+Vue.component('GridLayout', GridLayout)
+Vue.component('Button', Button)
+Vue.component('Img', Img)
+Vue.component('Label', Label)
 
 Vue.config.productionTip = false
 
 new Vue({
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
